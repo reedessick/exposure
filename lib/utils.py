d81d8a070b1ec3bc355017b02cb8af09283b7a95
Reed Essick <reed.essick@ligo.org>
2017-09-27 13:25:30 -0700

    now have a working version of compute_psd

--- lib/utils.py
+++ lib/utils.py
@@ -8,7 +8,10 @@ from pylal import Fr
 
 #-------------------------------------------------
 
-def vec_from_frames(frames, start, stop, verbose=False):
+def extract_start_dur(path, suffix='.gwf'):
+    return [int(_) for _ in path[:-len(suffix)].split('-')[-2:]]
+
+def vec_from_frames(frames, channel, start, stop, verbose=False):
         """
         returns a numpy array of the data inculded in frames between start and stop
         CURRENTLY ASSUME CONTIGUOUS DATA, but we should check this
@@ -22,7 +25,7 @@ def vec_from_frames(frames, start, stop, verbose=False):
                     print( frame )
                 s = max(strt, start)
                 d = min(start+dur,stop) - s
-                vec, gpstart, offset, dt, _, _ = Fr.frgetvect1d(frame, ifo_chan, start=s, span=d)
+                vec, gpstart, offset, dt, _, _ = Fr.frgetvect1d(frame, channel, start=s, span=d)
                 vecs.append( vec )
         vec = np.concatenate(vecs)
         return vec, dt
