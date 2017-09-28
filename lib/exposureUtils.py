ad5893b08b74de898e63c6c7295b52e5913a6319
Reed Essick <reed.essick@ligo.org>
2017-09-28 16:17:14 -0700

    a lot of debugging and shuffling to get condor-compute_psd_horizon_sensitivity up and running. NOTE: I have not really tested condor-compute_horizon or condor-compute_network_sensitivity, but I see those as sorta secondary at this point...

--- lib/exposureUtils.py
+++ lib/exposureUtils.py
@@ -6,6 +6,149 @@ __author__ = "Reed Essick (reed.essick@ligo.org)"
 import numpy as np
 from pylal import Fr
 
+import subprocess as sp
+
+#-------------------------------------------------
+
+### define template strings here for easy reference between multiple scripts!
+compute_horizon_sub = '''\
+universe = %(universe)s
+executable = %(exe)s
+arguments = "$(path) --output-dir %(outdir)s $(tag) --flow %(flow)d --fhigh %(fhigh)d --distance %(distance)f --snr-thr %(snr_thr)f -v"
+getenv = true
+accounting_group = %(accounting_group)s
+accounting_group_user = %(accounting_group_user)s
+log    = %(outdir)s/condor-compute_horizon%(filetag)s_$(Cluster)-$(Process).log
+error  = %(outdir)s/condor-compute_horizon%(filetag)s_$(Cluster)-$(Process).err 
+output = %(outdir)s/condor-compute_horizon%(filetag)s_$(Cluster)-$(Process).out
+notification = never
+queue 1'''
+
+compute_horizon_dag = '''\
+JOB    compute_horizon_%(jobid)s %(sub)s
+VARS   compute_horizon_%(jobid)s path="%(path)s" tag="%(tag)s"
+RETRY  compute_horizon_%(jobid)s %(retry)d
+'''
+
+compute_network_sensitivity_sub = '''\
+universe = %(universe)s
+executable = %(exe)s
+arguments = "$(gps) $(ifo_horizons) --nside %(nside)d --output-dir %(outdir)s --tag $(tag) -V"
+getenv = true
+accounting_group = %(accounting_group)s
+accounting_group_user = %(accounting_group_user)s
+log    = %(outdir)s/condor-compute_network_sensitivity%(filetag)s_$(Cluster)-$(Process).log
+error  = %(outdir)s/condor-compute_network_sensitivity%(filetag)s_$(Cluster)-$(Process).err 
+output = %(outdir)s/condor-compute_network_sensitivity%(filetag)s_$(Cluster)-$(Process).out
+notification = never
+queue 1'''
+
+compute_network_sensitivity_dag = '''\
+JOB    compute_network_sensitivity_%(jobid)s %(sub)s
+VARS   compute_network_sensitivity_%(jobid)s gps="%(gps)d" tag="%(tag)s" ifo_horizons="%(ifo_horizons)s"
+RETRY  compute_network_sensitivity_%(jobid)s %(retry)d
+'''
+
+compute_psd_sub = '''\
+universe = %(universe)s
+executable = %(exe)s
+arguments = "%(channel)s %(frametype)s $(gpsstart) $(gpsstop) --seglen %(seglen)d --overlap %(overlap)d --tukey-alpha %(tukey_alpha)f --output-dir %(outdir)s %(tag)s -V"
+getenv = true
+accounting_group = %(accounting_group)s
+accounting_group_user = %(accounting_group_user)s
+log    = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).log
+error  = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).err 
+output = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).out
+notification = never
+queue 1'''
+
+compute_psd_dag = '''\
+JOB    compute_psd_%(jobid)s %(sub)s
+VARS   compute_psd_%(jobid)s gpsstart="%(gpsstart)d" gpsstop="%(gpsstop)d"
+RETRY  compute_psd_%(jobid)s %(retry)d
+'''
+
+plot_psd_sub = '''\
+universe = %(universe)s
+executable = %(exe)s
+arguments = "$(psd) --f-min %(fmin)d --f-max %(fmax)d --y-min %(ymin)e --y-max %(ymax)e --output-dir %(outdir)s %(tag)s -v"
+getenv = true
+accounting_group = %(accounting_group)s
+accounting_group_user = %(accounting_group_user)s
+log    = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).log
+error  = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).err 
+output = %(outdir)s/condor-compute_psd%(filetag)s_$(Cluster)-$(Process).out
+notification = never
+queue 1'''
+
+plot_psd_dag = '''\
+JOB    plot_psd_%(jobid)s %(sub)s
+VARS   plot_psd_%(jobid)s psd="%(path)s"
+RETRY  plot_psd_%(jobid)s %(retry)d
+'''
+
+compute_exposure_sub = '''\
+universe = %(universe)s
+executable = %(exe)s
+arguments = "$(FITS) --nside %(nside)d --output-dir %(outdir)s %(tag)s"
+getenv = true
+accounting_group = %(accounting_group)s
+accounting_group_user = %(accounting_group_user)s
+log    = %(outdir)s/condor-compute_exposure%(filetag)s_$(Cluster)-$(Process).log
+error  = %(outdir)s/condor-compute_exposure%(filetag)s_$(Cluster)-$(Process).err 
+output = %(outdir)s/condor-compute_exposure%(filetag)s_$(Cluster)-$(Process).out
+notification = never
+queue 1'''
+
+compute_exposure_dag = '''\
+JOB    compute_exposure_%(jobid)s %(sub)s
+VARS   compute_exposure_%(jobid)s FITS="%(FITS)s"
+RETRY  compute_exposure_%(jobid)s %(retry)d
+'''
+
+query_cmd = "ligolw_segment_query_dqsegdb -q -t https://segments.ligo.org -a %(flag)s -s %(gpsstart)d -e %(gpsstop)d"
+print_cmd = "ligolw_print -c start_time -c end_time -t segment".split()
+
+#-------------------------------------------------
+
+def psd_path(output_dir, tag, gpsstart, gpsdur):
+    return "%s/psd%s-%d-%d.txt.gz"%(output_dir, tag, gpsstart, gpsdur)
+
+def horizon_path(output_dir, tag, gpstart, gpsdur):
+    return "%s/horizon%s-%d-%d.txt.gz"%(output_dir, tag, gpstart, gpsdur)
+
+def sensitivity_path(output_dir, tag, gps, gzip=False):
+    ans = "%s/network_sensitivity%s-%d.fits"%(output_dir, tag, gps)
+    if gzip:
+        ans = ans+".gz"
+    return ans
+
+#-------------------------------------------------
+
+def include_flags(segments, flags, optDict, verbose=False):
+    for flag in flags:
+        optDict['flag'] = flag
+        cmd = query_cmd%optDict
+        if verbose:
+            print( cmd )
+        segs = sp.Popen(cmd.split(), stdout=sp.PIPE).communicate()[0]
+        segs = [[int(_) for _ in seg.split(',')] for seg in sp.Popen(print_cmd, stdin=sp.PIPE, stdout=sp.PIPE).communicate(segs)[0].strip('\n').split('\n')]
+
+        segments = andsegments(segments, segs)
+    return segments
+
+def exclude_flags(segments, flags, optDict, verbose=False):
+    for flag in flags:
+        optDict['flag'] = flag
+        cmd = query_cmd%optDict
+        if verbose:
+            print( cmd )
+        segs = sp.Popen(cmd.split(), stdout=sp.PIPE).communicate()[0]
+        segs = [[int(_) for _ in seg.split(',')] for seg in sp.Popen(print_cmd, stdin=sp.PIPE, stdout=sp.PIPE).communicate(segs)[0].strip('\n').split('\n')]
+
+        segments = utils.andsegments(segments, utils.invsegments(gpsstart, gpsstop, segs))
+    return segments
+
 #-------------------------------------------------
 
 def extract_start_dur(path, suffix='.gwf'):
